563ae5f194faf8a35a904eff5906f3e08dddc367
henriquenovais <33753859+henriquenovais@users.noreply.github.com>
2019-02-13 14:50:18 -0300

    Implement "Hello World"

---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,22 @@
+﻿// This part is bascalyl
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Henrique"; //Declaring string
+            int characterAge; // Declaring int
+            characterAge = 24; 
+            Console.Write("Name:" + characterName + "\n");
+            Console.WriteLine("Age:" + characterAge + "\n");
+            Console.ReadLine();
+        }
+    }
+}
