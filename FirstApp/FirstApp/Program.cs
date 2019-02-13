66bf2e0bd020d84c6dd3eaf485801769d6a47047
henriquenovais <33753859+henriquenovais@users.noreply.github.com>
2019-02-13 15:12:27 -0300

    Implement user input

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -11,14 +11,29 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
+
+            /*
+            
+            Basic data manipulation:
+
             string characterName = "Henrique"; //Declaring string
             int characterAge; // Declaring int
             //float, double[most cases], decimal[insane precision]
             double gpa = 2.0;
             bool isMale = true; //Boolean
-            characterAge = 24; 
-            Console.WriteLine("Name:" + characterName);
-            Console.WriteLine("Age:" + characterAge);
+            characterAge = 24;
+            string phrase1 = "Name:";
+            string phrase2 = "Age:";
+            Console.WriteLine(phrase1 + characterName);
+            Console.WriteLine(phrase2 + characterAge);
+            //It is possible to manipulate strings as an array. 
+            //Ex: string[0]
+            Console.ReadLine();
+            */
+            //Simple user input:
+            Console.Write("Enter your ID: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("The name you typed was: " + userInput);
             Console.ReadLine();
         }
     }
